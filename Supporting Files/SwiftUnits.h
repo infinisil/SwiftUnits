51f97a36f45a4c9eefda0e8b178ae6b0094e59b1
Silvan Mosberger <infinisil@icloud.com>
2016-08-03 17:56:10 +0200

    Reorganized folder structure and added iOS, tvOS, watchOS framework targets

---
+++ Supporting Files/SwiftUnits.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUnits.h
+//  SwiftUnits
+//
+//  Created by Silvan Mosberger on 31/07/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUnits.
+FOUNDATION_EXPORT double SwiftUnitsVersionNumber;
+
+//! Project version string for SwiftUnits.
+FOUNDATION_EXPORT const unsigned char SwiftUnitsVersionString[];
+
+
+

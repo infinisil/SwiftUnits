3380e0abb44f7af5dcb905f4581aa729ad5c02a6
Silvan Mosberger <infinisil@icloud.com>
2016-08-03 18:23:52 +0200

    Added basic docs to operator implementations

--- Source/Operators.swift
+++ Source/Operators.swift
@@ -17,30 +17,53 @@ precedence 171
 }
 
 
+/**
+Adds two values together.
 
+- Requires: `l.dim` == `r.dim`, otherwise an assertion runtime error will occur.
+*/
 public func +<A:ValueProtocol, B:ValueProtocol>(l: A, r: B) -> Value {
 	assert(l.dim == r.dim, "Dimension mismatch: \(l.dim) vs \(r.dim)")
 	return Value(value: l.value + r.value, dim: l.dim)
 }
 
+/**
+Subtracts `r` from `l`
+
+- Requires: `l.dim` == `r.dim`, otherwise an assertion runtime error will occur.
+*/
 public func -<A:ValueProtocol, B:ValueProtocol>(l: A, r: B) -> Value {
 	assert(l.dim == r.dim, "Dimension mismatch: \(l.dim) vs \(r.dim)")
 	return Value(value: l.value - r.value, dim: l.dim)
 }
 
+/**
+Multiplies two values together.
+*/
 public func *<A:ValueProtocol, B:ValueProtocol>(l: A, r: B) -> Value {
 	return Value(value: l.value * r.value, dim: l.dim + r.dim)
 }
 
+/**
+Divides `l` by `r`
+*/
 public func /<A:ValueProtocol, B:ValueProtocol>(l: A, r: B) -> Value {
 	return Value(value: l.value / r.value, dim: l.dim - r.dim)
 }
 
+/**
+Calculates `l` to the power of `r`.
+
+- Requires: `l.dim * r.numerator` to be a multiple of `r.denominator`
+*/
 public func **<B: ValueProtocol>(l: B, r: Ratio) -> Value {
 	return Value(value: pow(l.value, r.value), dim: l.dim * r.numerator / r.denominator)
 }
 
-public func %%(nom: Int8, denom: Int8) -> Ratio {
-	return Ratio(numerator: nom, denominator: denom)
+/**
+Creates a ratio representing `num` / `denom`
+*/
+public func %%(num: Int8, denom: Int8) -> Ratio {
+	return Ratio(numerator: num, denominator: denom)
 }
 
